6defcd3a1b6dfa5b43b5c131c184e55dea95767a
Numedia <angelhananehanane@gmail.com>
2026-03-08 22:27:07 +0000

    Add process tree example with fork and loop
    Implement process tree creation with fork in a loop.

--- Gestion des processus/Exercices/exercice7.c
+++ Gestion des processus/Exercices/exercice7.c
@@ -1,1 +1,27 @@
+/*
+Exercice 7 — Arbre de processus avec une boucle
+Avec une boucle qui appelle fork() plusieurs fois :
 
+Dessine l'arbre de processus
+Détermine combien de processus sont créés
+Explique le rôle du break
+Analyse le comportement de l'ordonnancement
+  */
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        pid_t pid = fork();
+
+        if (pid == 0)
+        {
+            printf("fils  : PID=%d\n", getpid());
+            break;  // le fils sort de la boucle
+        }
+        else
+            printf("pere  : PID=%d a cree fils=%d\n", getpid(), pid);
+    }
+    return 0;
