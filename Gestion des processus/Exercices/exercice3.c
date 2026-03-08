dce9cad195ba69bb56b67b406d9c90fc44cb03ae
Numedia <angelhananehanane@gmail.com>
2026-03-08 22:06:18 +0000

    Update exercice3.c

--- Gestion des processus/Exercices/exercice3.c
+++ Gestion des processus/Exercices/exercice3.c
@@ -6,3 +6,25 @@ Ajoute un sleep() dans le père et dans le fils
 Utilise la commande ps pour voir les deux processus en même temps
 Décris ce que tu observes sur le PID et le PPID
 */
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        sleep(10);  // ← ajoute ça
+        printf("fils : fork() = %d\n", pid);
+    }
+    else if (pid > 0)
+    {
+        sleep(10);  // ← ajoute ça
+        printf("pere : fork() = %d\n", pid);
+    }
+    else
+        printf("erreur fork\n");
+    return 0;
+}
